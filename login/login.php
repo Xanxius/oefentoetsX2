46169885abb7e7167d958aa2beb9d9cd25b89676
Xanxius <kenai.freriks@student.graafschapcollege.nl>
2023-04-06 09:18:24 +0200

    git push 1

--- login/login.php
+++ login/login.php
@@ -20,7 +20,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
+        $username_err = "Please enter userName.";
     } else{
         $username = trim($_POST["username"]);
     }
